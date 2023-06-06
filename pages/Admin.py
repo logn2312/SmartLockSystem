ed9de2965b5d8860b1caadb4497934ef66b205e0
logn2312 <75211558+logn2312@users.noreply.github.com>
2023-06-07 04:52:32 +0700

    Update Admin.py

--- pages/Admin.py
+++ pages/Admin.py
@@ -3,9 +3,42 @@ from streamlit_extras.switch_page_button import switch_page
 import base64
 import requests
 from streamlit_lottie import st_lottie
+from streamlit_option_menu import option_menu
 from PIL import Image
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import csv
+import os
+import pandas as pd
 import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
+import time
+import datetime
+
+import yagmail
+import smtplib
+import imaplib
+
+#setup broker mqtt
+BROKER = 'broker.hivemq.com'
+PORT = 1883
+TOPIC1 = 'door1'
+TOPIC2 = 'door2'
+
+#Login to the mail account through IMAP and SMTP
+
+imap = imaplib.IMAP4_SSL('imap.gmail.com',993)                                          
+unm = 'longdht.2312@gmail.com'                                                      
+psw = 'dtrihdiqdebjzaqo'                                                                 
+imap.login(unm,psw) 
+
+mail=smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login(unm,psw)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected")
 
 st.set_page_config(page_title="Homepage",layout="wide")
 
@@ -78,14 +111,61 @@ def load_lottieurl(url):
     return r.json()
 load_coding = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_6e0qqtpa.json")
 
+def upload_file_to_google_sheet(file_path, spreadsheet_id, sheet_name):
+    # Đọc thông tin xác thực từ file JSON
+    credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", ["https://www.googleapis.com/auth/spreadsheets"])
+
+    # Tạo kết nối đến Google Sheets API
+    client = gspread.authorize(credentials)
+
+    # Mở spreadsheet
+    spreadsheet = client.open_by_key(spreadsheet_id)
+
+    # Lấy sheet theo tên
+    sheet = spreadsheet.worksheet(sheet_name)
+
+    # Đọc dữ liệu từ file và ghi vào sheet
+    with open(file_path, "r") as file:
+        data = pd.read_csv(file)
+
+    # Tạo danh sách dữ liệu từ tệp
+    values = data.values.tolist()
+    
+    # Gửi dữ liệu lên sheet
+    sheet.insert_rows(values)
+
+def automail():
+    recipient = "ITITIU19026@student.hcmiu.edu.vn"
+    date = datetime.date.today().strftime("%B %d, %Y")
+    # path = 'Attendance'
+    # os.chdir(path)
+    # files = sorted(os.listdir(os.getcwd()), key=os.path.getmtime)
+    # newest = files[-1]
+    # filename = newest
+    sub = "Attendance Report for " + str(date)
+    yag = yagmail.SMTP(unm, psw)
+    yag.send(
+        to=recipient, #recipient email ID
+        subject=sub, # email subject
+        contents='''Dear Lecturer,
+
+        Here is the Attendance details of your class. Please have a look at the file attached to get more information. If you have any questions or concerns, don't hesitate to contact me during my leave via email or phone.
+                
+        Thank you & Best Regards,
+
+        Office of Undergraduate Academic Affairs.
+        ''',  # email body
+        attachments= "Attendance.csv"  # file attached
+    )
+
 image1 = Image.open('logo.png')
 image2 = Image.open('robot.png')
 
 with st.container():
-    col1, col2 = st.columns(2)
+    col1, col2 = st.columns([1,1.5])
     with col2:
         with st.container():
-            col5, col6, col7, col8, col9, col10 = st.columns(6)
+            col5, col6, col7, col8, col9 = st.columns(5)
             with col5:
                 st.text("")
                 st.text("")
@@ -103,28 +183,120 @@ with st.container():
                 st.text("")
                 st.text("")
                 about_us = st.button('**About Us**')
-                
             with col9:
                 st.text("")
                 st.text("")
                 contact = st.button('**Contact Us**')
-                st.text("")
-                st.text("")
-                st.image(image2, use_column_width = 'always')
-            with col10:
-                st.text("")
-                st.text("")
-                contact = st.button('**Login**')
+        with st.container():
+            #room1, room2 = st.columns(2)
+            #with room1:
+                #with st.container():
+            st.write('**Room Status**')
+            placeholder1 = st.empty()
+            remote_control1 = st.selectbox("**Remote Control Room Door Lock**", ('','Unlock✅','Lock🚨'))
+            if remote_control1 == 'Unlock✅':
+                led_state = 'ON'
+                publish.single(TOPIC1, "Unlock", hostname=BROKER, port=PORT)
+                placeholder1.success('Unlock!', icon="✅")
+            elif remote_control1 == 'Lock🚨':
+                led_state = 'OFF'
+                publish.single(TOPIC1, "Lock", hostname=BROKER, port=PORT)
+                placeholder1.error('Lock!', icon="🚨")
+
+                
+                    
+            # with room2:
+            #     with st.container():
+            #         st.write('**Room 2**')
+            #         placeholder2 = st.empty()
+            #         remote_control2 = st.selectbox("**Remote Control Room 2**", ('','Unlock✅','Lock🚨'))
+
+            #         if remote_control2 == 'Unlock✅':
+            #             publish.single(TOPIC2, "Unlock", hostname=BROKER, port=PORT)
+            #         elif remote_control2 == 'Lock🚨':
+            #             publish.single(TOPIC2, "Lock", hostname=BROKER, port=PORT)
+            #placeholder1.warning("")
+            #placeholder2.warning("")
+
+            def on_message(client, userdata, message):
+                msg = message.payload.decode()
+                if msg == 'Unlock':
+                    placeholder1.success('Unlock!', icon="✅")
+                    time.sleep(5)
+                elif msg == 'Lock':
+                    placeholder1.error('Lock!', icon="🚨")
+                    time.sleep(1)
+                #publish.single(TOPIC1, led_state, hostname=BROKER, port=PORT)
+        placeholder3 = st.empty()
+        placeholder4 = st.empty()
+        # with st.container():
+        #     func1,func2,func3,func4= st.columns(4)
+        #     with func1:
+        #         upload = st.button('Upload')
+        #     with func2:
+        #         email = st.button('Email')
+        #     with func3:
+        #         history = st.button('History')
+        #     with func4:
+        #         stat = st.button('Statistics') 
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Upload", "Email", "History"],#, "Statistics"],  # required
+            icons=["upload", "envelope", "clock-history"],# "activity"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "red"},
+                "nav-link": {
+                    "text-align": "center",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "#D5C9FE", "color": "#10266f"},
+            }
+        )
+        
+        if selected == 'Email':
+            automail()
+            placeholder3.success('Email Successfully Sent✅')
+        elif selected == 'History':
+            df = pd.read_csv("Attendance.csv")
+            placeholder3.dataframe(df)
+        # elif selected == 'Statistics':
+        #     placeholder3.line_chart({"data": [1, 5, 2, 6]})
+        # Upload file lên Google Sheets
+        elif selected == 'Upload':
+            uploaded_file = placeholder3.file_uploader("Chọn file để tải lên")
+
+            if uploaded_file is not None:
+                # Read CSV file into a Pandas DataFrame
+                df = pd.read_csv(uploaded_file)
+                df.to_csv('file.csv', index= False)
+                spread_sheet_id = '10hRdVBjcUzJ5l-U6Ww27gFqGSh4BCyNirHJCKq8ufK0'
+                sheet_name = 'Sheet1'  # Tên sheet trong Google Sheets
+                upload_file_to_google_sheet('file.csv', spread_sheet_id, sheet_name)
+                placeholder4.success('Upload file successfully!')
+
     with col1:
         col3, col4, _ = st.columns([1,3,1])
         with col3:
-            st.image(image1, width = 110)
+            st.image(image1, width = 100)
+        st.title("Smart Lock System")
+        st_lottie(load_coding, width = 350, height = 200,key="coding")
+        st.header("Welcome to my app :heart:")
+        if about_us:
+            st.header('About Us')
+            st.info('This Smart Lock System Based System is designed by International University!')
+        elif contact:
+            st.header('Contact Us')
+            st.info('If you find you need any help contact me on pdtdh@hcmiu.edu.vn!')
+
         with col4:
             st.text("")
             st.write("VIETNAM NATIONAL UNIVERSITY HCMC")
             st.write("**INTERNATIONAL UNIVERSITY**")
-            st.title("Smart Lock System")
-            st_lottie(load_coding, width = 300, height = 200,key="coding")
             # Add the main content
             if home:
                 switch_page("Homepage")
@@ -132,30 +304,12 @@ with st.container():
                 switch_page("Admin")
             elif user:
                 switch_page("User")
-            elif about_us:
-                st.header('About Us')
-                st.info('This Smart Lock System Based System is designed by International University!')
-            elif contact:
-                st.header('Contact Us')
-                st.info('If you find you need any help contact me on pdtdh@hcmiu.edu.vn!')
 
-with st.container():
-    login_col, func_col = st.columns(2)
-    with login_col:
-        _, _ , login_col2, _, _= st.columns(5)
-        with login_col2:
-            st.button('Login')
-    with func_col:
-        col1, col2 = st.columns(2)
-        with col1:
-            if st.button('Room 1'):
-                st.success("**Unlock**")
-                placeholder = st.empty()
-                placeholder.line_chart({"data": [1, 5, 2, 6]})
-            else:
-                st.error("**Lock**")
-        with col2:
-            if st.button('Room 2'):
-                st.success("**Unlock**")
-            else:
-                st.error("**Lock**")
+client = mqtt.Client()
+client.connect(BROKER, PORT)
+client.subscribe(TOPIC1)
+client.subscribe(TOPIC2)
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.loop_forever()
